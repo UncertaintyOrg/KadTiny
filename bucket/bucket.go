eed48252ae96b1e03ea3ddc3ed8b102d5d47d031
henry <hq.liu@qq.com>
2020-03-15 16:55:48 +0800

    arch

---
+++ bucket/bucket.go
@@ -0,0 +1,21 @@
+package bucket
+
+type RouterItem struct {
+
+}
+
+type Router struct {
+	items []RouterItem
+}
+
+type Bucket struct {
+	kParam int
+	router Router
+}
+
+// bucket constructor
+
+// add bucket item
+
+// delete bucket item
+
